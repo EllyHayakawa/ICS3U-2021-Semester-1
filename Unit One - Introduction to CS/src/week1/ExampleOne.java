6690afe0d13fc434f00fd8b8710892ed9a58be79
Elly Hayakawa <ehayakawa@bayviewglen.ca>
2021-09-09 09:29:58 -0400

    Initial Commit - Unit One

---
+++ Unit One - Introduction to CS/src/week1/ExampleOne.java
@@ -0,0 +1,17 @@
+package Week1;
+
+
+// Every file will be its own class FOR NOW
+public class ExampleOne {
+
+    public static void main(String[] args) {
+        //the main method (function) let's Java know where to start.
+
+        //System.out.println writes to the console
+        //Whatever is in the round brackets goes in the console.
+        //double click to highlight word, triple click to highlight line
+        System.out.println("Welcome to ICS 3U - AP");
+        System.out.println("Welcome to ICS 3U - AP");
+        System.out.println("Welcome to ICS 3U - AP");
+    }
+}
