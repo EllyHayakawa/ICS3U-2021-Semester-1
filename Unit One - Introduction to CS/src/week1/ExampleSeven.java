3acfbfe49104814bff8366d555c6677d375e85f9
Elly Hayakawa <ehayakawa@bayviewglen.ca>
2021-09-13 09:29:23 -0400

    Compound Assignment, Final Modifier, Type Casting

---
+++ Unit One - Introduction to CS/src/week1/ExampleSeven.java
@@ -0,0 +1,30 @@
+package week1;
+
+/**
+ * Compound Assignment Operators
+ */
+public class ExampleSeven {
+    public static void main(String[] args) {
+        int x = 1;
+
+        //x = x + 7; // x is assigned x + 7 (add 7 to x and assign back to x)
+        x += 7; //8
+        x -= 3; //5
+        x *= 2; //10
+        x /=3; //3
+        x %= 4; //3
+
+        x++;
+        ++x;
+        x--;
+        --x;
+
+        //where x = 3
+        int y = 2 * x++; // (y = 2*x), then increment x (x = 4)
+        int z = 2 * ++x; // y = 2*(x+1)
+        
+        y+=2;
+        x+=3;
+
+    }
+}
---
+++ Unit One - Introduction to CS/src/week1/ExampleSix.java
@@ -0,0 +1,16 @@
+package week1;
+
+/**
+ * final modifier - used to make a variable constant
+ */
+public class ExampleSix {
+    public static void main(String[] args) {
+        final int x = 7;
+        //x = 8;   syntax because we cannot reassign a value to a variable that has been modified with final
+        final int y;
+        y = x;  // no issue because this is the first time we are assigning a value to y
+
+        final int NUMBER_OF_STUDENTS = 30;  //naming convention of constants is all caps and _ for spaces
+       
+    }
+}
