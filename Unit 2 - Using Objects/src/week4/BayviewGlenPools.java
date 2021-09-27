73d334067b11b6898beafb5cb769d7ea28572401
Elly Hayakawa <ehayakawa@bayviewglen.ca>
2021-09-27 10:19:12 -0400

    BVG Pool Assignment

---
+++ Unit 2 - Using Objects/src/week4/BayviewGlenPools.java
@@ -0,0 +1,38 @@
+package week4;
+
+public class BayviewGlenPools {
+    public static void main(String[] args) {
+        final int length = 20;
+        final int width = 8;
+        final int shallowLength = 5;
+        final int transition = 7;
+        final int shallowHeight = 3;
+        final int deepHeight = 8;
+
+        final int linerCost = 2;
+
+        int volume = calculateVolume(length, width, shallowLength, shallowHeight, transition, deepHeight);
+        int surfaceArea = calculateSurfaceArea(length, width, shallowLength, shallowHeight, transition, deepHeight);
+    
+        int n = (int)(volume*0.9);
+        double costLiner = (double)((int)((surfaceArea*(double)linerCost)*100))/100;
+        System.out.println(n);
+        System.out.println(surfaceArea);
+        System.out.println("The cost of the liner is $" + costLiner);
+        
+    }
+
+    private static int calculateVolume(int length, int width, int shallowLength, int shallowHeight, int transition,
+            int deepHeight) {
+        int volume = (int)((shallowLength*shallowHeight*width)+(deepHeight*(20-5-Math.sqrt(24))*width)+(shallowHeight*width*(Math.sqrt(24)))+((5*(Math.sqrt(24)/2))*width));
+        return volume;
+    }
+
+    private static int calculateSurfaceArea(int length, int width, int shallowLength, int shallowHeight, int transition,
+            int deepHeight) {
+        final double deepLength = (15-(Math.sqrt(24)));
+        int surfaceArea = (int)((shallowHeight*width)+(deepHeight*width)+(shallowLength*shallowHeight*2)+(shallowLength*width)+(transition*width)+(deepLength*width)+(deepLength*deepHeight*2)+(((shallowHeight+deepHeight)/2.0)*(Math.sqrt(24))));
+        return surfaceArea;
+    }
+    
+}
